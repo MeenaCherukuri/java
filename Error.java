eda2d1145bb5af0e6d36531dd4619efc363a7eca
Meenakshi <143991085+MeenaCherukuri@users.noreply.github.com>
2023-10-19 15:41:16 +0530

    Create Error.java

---
+++ Error.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+import java.awt.*;
+public void paint(Graphics g)
+{
+    g.drawstring("Piyush bajpai",100,100);
+}
+<html>
+<title>applet</title>
+<applet code = "Piyush.class"width=400 height=400></applet>
+</html>
